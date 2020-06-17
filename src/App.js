4141b6eeca6a00a8d688ca44badcca90da9421f5
Jesse Solis <jesse.solis0316@gmail.com>
2020-06-17 14:42:04 -0500

    setup Auth0

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react';
+// src/App.js
+
+import React from "react";
+import NavBar from "./components/NavBar";
+
+// New - import the React Router components, and the Profile page component
+import { Router, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import history from "./utils/history";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
-    <h1>Hello world!</h1>
+    <div className="App">
+      {/* Don't forget to include the history module */}
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
