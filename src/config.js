8767a4be226f337de0cef6e7c2ed63c453d2201d
Jesse Solis <jesse.solis0316@gmail.com>
2020-06-30 11:29:53 -0500

    heroku deployment changes

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ module.exports = {
     api:
         process.env.NODE_ENV === "development"
             ? "http://localhost:5000"
-            : "",
+            : "https://health-nut-api.herokuapp.com/",
 };
